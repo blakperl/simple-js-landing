f21cc8f11d862e84e28b6d0e4eafb8e673da35fe
blakperl <owolabirukayat7@gmail.com>
2024-01-02 15:25:19 +0100

    add smooth animation

--- landing/script.js
+++ landing/script.js
@@ -12,6 +12,29 @@
 //   })
 // })()
 
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+
 (function() {
   let landingNav = document.getElementById("landingNav");
   let landingBtn = document.getElementById("navbutton");
